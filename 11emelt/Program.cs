b870b8c5338a8cc11ec32d5df0cd128450122444
knnorbi <kluka.norbert@gmail.com>
2020-11-26 13:49:34 +0100

    Initial commit

---
+++ 11emelt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11emelt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle h1 = new Triangle(1, 2, 3);
+            h1.SetSides(2, -2, 2);
+            Console.WriteLine(h1.Editable());
+            Console.WriteLine(h1.Perimeter());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 11emelt/Triangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11emelt
+{
+    class Triangle
+    {
+        int a;
+        int b;
+        int c;
+
+        public Triangle(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void SetSides(int _a, int _b, int _c)
+        {
+            if (_a <= 0 || _b <= 0 || _c <= 0 || !IsValid(_a, _b, _c))
+            {
+                return;
+            }
+
+            a = _a;
+            b = _b;
+            c = _c;
+        }
+
+        public int Perimeter()
+        {
+            return a + b + c;
+        }
+
+        public bool IsValid()
+        {
+            return IsValid(a, b, c);
+        }
+
+        public bool IsValid(int a, int b, int c)
+        {
+            bool aValid = a > b + c;
+            bool bValid = b > a + c;
+            bool cValid = c > b + a;
+
+            return aValid && bValid && cValid;
+        }
+
+    }
+}
