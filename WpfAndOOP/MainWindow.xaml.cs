7d142c966e89df9052153144834abfdcacd4c5b6
knnorbi <kluka.norbert@gmail.com>
2020-12-10 11:24:16 +0100

    20201210

---
+++ WpfAndOOP/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAndOOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            aTb.Text = string.Empty;
+            bTb.Text = string.Empty;
+            cTb.Text = string.Empty;
+        }
+
+        private void newBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //List<int> lista = new List<int>();
+            //lista.Add(2);
+            //lista.Add(5);
+            //lista.Add(324);
+            //Console.WriteLine(lista[2]);
+            //for (int i = 0; i < lista.Count; i++)
+            //{
+            //    Console.WriteLine(lista[i]);
+            //}
+            //lista.RemoveAt(1);
+
+            lb.Items.Add(new Triangle(1,1,1));
+        }
+
+        private void lb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Triangle triangle = lb.Items[lb.SelectedIndex] as Triangle;
+
+            aTb.Text = triangle.A.ToString();
+            bTb.Text = triangle.B.ToString();
+            cTb.Text = triangle.C.ToString();
+
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Triangle triangle = lb.Items[lb.SelectedIndex] as Triangle;
+
+            triangle.A = int.Parse(aTb.Text);
+            triangle.B = int.Parse(bTb.Text);
+            triangle.C = int.Parse(cTb.Text);
+            lb.Items.Refresh();
+        }
+    }
+}
---
+++ WpfAndOOP/Triangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAndOOP
+{
+    class Triangle
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public Triangle(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public void SetSides(int _a, int _b, int _c)
+        {
+            if (_a <= 0 || _b <= 0 || _c <= 0 || !IsValid(_a, _b, _c))
+            {
+                return;
+            }
+
+            A = _a;
+            B = _b;
+            C = _c;
+        }
+
+        public int Perimeter
+        {
+            get 
+            {
+                return A + B + C;
+            }
+        }
+
+        public bool IsValid()
+        {
+            return IsValid(A, B, C);
+        }
+
+        public bool IsValid(int a, int b, int c)
+        {
+            bool aValid = a > b + c;
+            bool bValid = b > a + c;
+            bool cValid = c > b + a;
+
+            return aValid && bValid && cValid;
+        }
+
+        public override string ToString()
+        {
+            return $"A:{A} B:{B} C:{C}";
+        }
+    }
+}
